3d54cbee5449d65db6fe3badeb65cfaf6dc260d8
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2021-06-23 20:48:36 +0200

    Add initial parsing of input

--- src/main/java/AntlrMockExamListener.java
+++ src/main/java/AntlrMockExamListener.java
@@ -1,6 +1,39 @@
+import lombok.Getter;
+
 public class AntlrMockExamListener extends MockExamBaseListener {
+    private final StringBuilder output = new StringBuilder();
+
+    public String getOutput() {
+        return output.toString();
+    }
+
     @Override
     public void exitDeclare_function(MockExamParser.Declare_functionContext ctx) {
-        System.out.println(ctx.getText());
+        output.append("FUNCTION: ")
+                .append(ctx.variable().getText())
+                .append(" ")
+                .append("#par=")
+                .append(ctx.type().size() - 1)
+                .append("\n");
+    }
+
+    @Override
+    public void exitDefine_constant(MockExamParser.Define_constantContext ctx) {
+        output.append("INTEGER: ")
+                .append("'")
+                .append(ctx.variable().getText())
+                .append("' = ")
+                .append(ctx.value().getText())
+                .append("\n");
+    }
+
+    @Override
+    public void enterAssertion(MockExamParser.AssertionContext ctx) {
+        output.append("ASSERT: ").append("(");
+    }
+
+    @Override
+    public void exitAssertion(MockExamParser.AssertionContext ctx) {
+        output.append(")\n");
     }
 }
