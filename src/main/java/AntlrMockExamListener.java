4b65b65e6dc4873cb01c33579d31abfd77a0da6e
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2021-06-24 11:28:55 +0200

    Add error handling for redefined const

--- src/main/java/AntlrMockExamListener.java
+++ src/main/java/AntlrMockExamListener.java
@@ -1,7 +1,9 @@
-import lombok.Getter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AntlrMockExamListener extends MockExamBaseListener {
     private final StringBuilder output = new StringBuilder();
+    private final List<String> variables = new ArrayList<>();
 
     public String getOutput() {
         return output.toString();
@@ -18,13 +20,27 @@ public class AntlrMockExamListener extends MockExamBaseListener {
     }
 
     @Override
-    public void exitDefine_constant(MockExamParser.Define_constantContext ctx) {
+    public void enterDefine_constant(MockExamParser.Define_constantContext ctx) {
         output.append("INTEGER: ")
                 .append("'")
                 .append(ctx.variable().getText())
                 .append("' = ")
                 .append(ctx.value().getText())
                 .append("\n");
+    }
+
+    @Override
+    public void exitDefine_constant(MockExamParser.Define_constantContext ctx) {
+        var variable = ctx.variable().getText();
+
+        if (variables.contains(variable)) {
+            output.append("*** ERROR in define-const: variable '")
+                    .append(variable)
+                    .append("' redefined ***")
+                    .append("\n");
+        } else {
+            variables.add(variable);
+        }
     }
 
     @Override
